80e53f82cfc4c57f21211ef265a335baf6cb8951
taufique1131 <161253675+taufique1131@users.noreply.github.com>
2025-04-08 16:09:06 +0530

    Update app.js

--- hello-world-app/app.js
+++ hello-world-app/app.js
@@ -1,16 +1,11 @@
 const express = require('express');
 const app = express();
-const PORT = 3000;
-
-app.get('/', (req, res) => {
-    res.send('Hello, World from EKS!');
-});
+const port = 3000;
 
 app.get('/hello', (req, res) => {
-    res.send('Hello, World from hello!');
+  res.send('Hello World!');
 });
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+app.listen(port, () => {
+  console.log(`Hello World app listening at http://localhost:${port}`);
 });
-
